1f8d6aa6daa67d7b916fcadf806df489910ea328
Konata <konata@vip.qq.com>
2014-09-15 03:01:38 +0800

    code change

--- src/cn/beriru/weicoadsremover/ReflectionUtils.java
+++ src/cn/beriru/weicoadsremover/ReflectionUtils.java
@@ -23,7 +23,12 @@ public class ReflectionUtils {
 	
 	public static void setObjectFields(Object target,Object newValue,String... fields) throws Throwable{
 		try{
-			String[] prefixes = (String[]) Arrays.asList(fields).subList(0, fields.length - 1).toArray();
+					
+			Object[] objs = Arrays.asList(fields).subList(0, fields.length - 1).toArray();
+			String[] prefixes = new String[objs.length];
+			for(int i = 0; i < objs.length; i++){
+				prefixes[i] = objs[i].toString();
+			}
 			String field = fields[fields.length - 1];
 			Object o = getObjectFields(target,prefixes);
 			XposedHelpers.setObjectField(o, field, newValue);
