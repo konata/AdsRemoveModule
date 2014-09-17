6bc3cdfaea7d03c4c101192090b82e9e7bf8dfc9
lolicon <beriru@qq.com>
2014-09-17 17:03:00 +0800

    hooking meizu checkLiscene

--- src/cn/beriru/weicoadsremover/AdsRemoverModule.java
+++ src/cn/beriru/weicoadsremover/AdsRemoverModule.java
@@ -5,6 +5,9 @@ import static cn.beriru.weicoadsremover.ReflectionUtils.log;
 import java.util.ArrayList;
 
 import android.os.Bundle;
+
+import com.meizu.mstore.license.LicenseResult;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -31,8 +34,21 @@ public class AdsRemoverModule implements IXposedHookLoadPackage {
 	}
 	
 	public static class MEIZU {
-		public static final String HOOK_CLASS_NAME = "";
+		public static final String HOOK_CLASSNAME_RESULT = "com.meizu.mstore.license.LicenseResult";
+		public static final String HOOK_CLASSNAME_CHECKER = "com.meizu.mstore.license.LicenseCheckHelper";
 		
+		public static final String HOOK_FUNCNAME_GETRESULT_CODE = "getResponseCode"; // (void)
+		public static final String HOOK_FUNCNAME_CHECKRESULT = "checkResult"; // (String ,LicenseResult)
+		public static final String HOOK_FUNCNAME_PURCHASETYPE = "getPurchaseType"; // (void)
+		
+		public static final int RESP_SUCC = 1;
+		public static final int PURCHASE_TYPE = 1;
+		
+		// LicenseResult result = mLicensingService.checkLicense(pkgName); 						    // hook to not null (skip)
+		// if( result != null && result.getResponseCode() == LicenseResult.RESPONSE_CODE_SUCCESS){  // hook getResponseCode == SUCC
+		// LicenseCheckHelper.checkResult(APP_KEY, result);  										// hook result to true
+		// result.getPurchaseType() == LicenseResult.PURCHASE_TYPE_NORMAL 							// hook type == normal
+
 	}
 	
 	@Override
@@ -84,7 +100,9 @@ public class AdsRemoverModule implements IXposedHookLoadPackage {
 					super.beforeHookedMethod(param);
 					log("done");
 				}
-			});
+			}); 
+			
+			
 			
 			/** hooking source
 			 * if(Math.abs(v0 - this.z) > 1000 && !this.D) {
@@ -118,30 +136,61 @@ public class AdsRemoverModule implements IXposedHookLoadPackage {
 				}
 			});
 		}
-		
+
+	
 		/**
 		 * meizu license result check hooking
 		 */
-		/*
-		try{
-			Class<?> LisceneCheckResult = XposedHelpers.findClass(MEIZU.HOOK_CLASS_NAME, lp.classLoader);
-			XposedHelpers.findAndHookMethod(LisceneCheckResult, MEIZU.HOOK_FUNCNAME_CHECK, );
-			XposedHelpers.findAndHookMethod(LisceneCheckResult, MEIZU.HOOK_, parameterTypesAndCallback)
-			
-			
-		}catch(ClassNotFoundError e){
-			log("hookmeizuapi4pkg:" + lp.packageName + " failed,no class found");
+		// LicenseResult result = mLicensingService.checkLicense(pkgName); 						    // hook to not null (skip)
+		// if( result != null && result.getResponseCode() == LicenseResult.RESPONSE_CODE_SUCCESS){  // hook getResponseCode == SUCC
+		// LicenseCheckHelper.checkResult(APP_KEY, result);  										// hook result to true
+		// result.getPurchaseType() == LicenseResult.PURCHASE_TYPE_NORMAL 							// hook type == normal
+		
+		if(shouldHookMeizuChecker(lp)){
+			try{
+				log("meizu hook begin");
+				Class<?> LicenseResult = XposedHelpers.findClass(MEIZU.HOOK_CLASSNAME_RESULT, lp.classLoader);
+				log("meizu hook getresult");
+				XposedHelpers.findAndHookMethod(LicenseResult, MEIZU.HOOK_FUNCNAME_GETRESULT_CODE, new XC_MethodHook(){
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						super.afterHookedMethod(param);
+						log("meizu hook getresult to 1");
+						param.setResult(MEIZU.RESP_SUCC);
+					}
+				});
+
+				log("meizu hook purchase type");
+				XposedHelpers.findAndHookMethod(LicenseResult, MEIZU.HOOK_FUNCNAME_PURCHASETYPE, new XC_MethodHook(){
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						super.afterHookedMethod(param);
+						log("meizu hook purchase type to 1");
+						param.setResult(MEIZU.PURCHASE_TYPE);
+					}
+				});
+
+				log("meizu hook checkresult");
+				XposedHelpers.findAndHookMethod(MEIZU.HOOK_CLASSNAME_CHECKER, lp.classLoader, MEIZU.HOOK_FUNCNAME_CHECKRESULT, String.class,LicenseResult,new XC_MethodHook(){
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						super.afterHookedMethod(param);
+						log("meizu hook checkresult to true");
+						param.setResult(true);
+					}
+				});
+			}catch(Exception e){
+				log(e);
+				throw e;
+			}
+			log("meizu hook end");
 		}
-		*/
 	}
-	
 	
 	public ArrayList<Object> filterAds(ArrayList<Object> statusList){
 		if(statusList == null){
 			return null;
 		}
-		
-		
 		ArrayList<Object> items = new ArrayList<Object>();
 		for(Object o : statusList){
 			Object result = XposedHelpers.callMethod(o, "isIsad", new Object[]{});
@@ -153,5 +202,15 @@ public class AdsRemoverModule implements IXposedHookLoadPackage {
 		return items;
 	}
 	
+	
+	public static boolean shouldHookMeizuChecker(LoadPackageParam param){
+		try{
+			XposedHelpers.findClass(MEIZU.HOOK_CLASSNAME_RESULT, param.classLoader);
+			log("begin hook meizu for package " + param.packageName + " ❤~~♪~~");
+			return true;
+		}catch(ClassNotFoundError cne){
+			return false;
+		}
+	}
 	
 }
